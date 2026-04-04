2d94a1ed5fd6072359990f0b405f4d21ce04ebaf
Nikunj <70919626+nem-junk@users.noreply.github.com>
2026-04-04 11:30:49 +0530

    Delegate,and struct in AttributeSet

--- Source/Whiplash/Private/Attributes/WAttributeSet.cpp
+++ Source/Whiplash/Private/Attributes/WAttributeSet.cpp
@@ -1,2 +1,10 @@
 ﻿
 #include "Attributes/WAttributeSet.h"
+
+
+UWorld* UWAttributeSet::GetWorld() const
+{
+	UObject* Outer = GetOuter();
+	check(Outer);
+	return Outer->GetWorld();
+}
--- Source/Whiplash/Public/Attributes/WAttributeSet.h
+++ Source/Whiplash/Public/Attributes/WAttributeSet.h
@@ -4,39 +4,36 @@
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
 #include "GameplayTags.h"
+#include "WAttributeDataStruct.h"
+
 ////////////////////////////////////
 #include "WAttributeSet.generated.h"
 ////////////////////////////////////
-USTRUCT()
-struct FWAttributeData
-{
-	GENERATED_BODY()
-
-	UPROPERTY()
-	FGameplayTag AffectedAttribute;  
-	UPROPERTY()
-	float BaseValue = 0.0f;          
-
-	UPROPERTY()
-	float CurrentValue = 0;    
-	
-	UPROPERTY()
-	float Modifiers= 0 ;
-
-	UPROPERTY()
-	FTimerHandle TimerHandle;       
-};
-
-
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnAttributeChange,float/*OldValue*/,float/*NewValue*/);
+DECLARE_MULTICAST_DELEGATE(FOnAttributeDepleted);
 
 UCLASS()
 class WHIPLASH_API UWAttributeSet : public UObject
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WAttributeSet|Health")
-	float Health = 100.f;
-	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WAttributeSet|Stamina")
-	float Stamina = 100.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Attribute|Health")
+	FAttributeData Health = FAttributeData(100);
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Attribute|Health")
+	FAttributeData MaxHealth = FAttributeData(100);
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Attribute|Stamina")
+	FAttributeData Stamina = FAttributeData(700);
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Attribute|Stamina")
+	FAttributeData MaxStamina = FAttributeData(700);
 	
+	FOnAttributeChange OnHealthChanged;
+	FOnAttributeChange OnMaxHealthChanged;
+	FOnAttributeChange OnStaminaChanged;
+	FOnAttributeDepleted  OnOutOfHealth;
+	FOnAttributeDepleted  OnOutOfStamina;
+	
+	
+	
+public:
+	UWorld* GetWorld() const override;
 };
