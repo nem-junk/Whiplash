48f9a496afe12d9216eaa4b36a8b985bdc5d23ed
Nikunj <70919626+nem-junk@users.noreply.github.com>
2026-04-11 14:32:42 +0530

    WeaponFunctions

---
+++ Source/Whiplash/Private/Weapons/WWeaponStateComponent.cpp
@@ -0,0 +1,50 @@
+﻿#include "Weapons/WWeaponStateComponent.h"
+#
+
+static const float StandingStillSpeedThreshold = 80.1f;
+
+UWWeaponStateComponent::UWWeaponStateComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+void UWWeaponStateComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+	WCachedPawn = Cast<>
+}
+
+void UWWeaponStateComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UWWeaponStateComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (!WeaponProperties)return;
+	bMinSpread=IsSpreadAtMinimum(DeltaTime);
+	bMinMultipliers=AreMultipliersAtMinimum(DeltaTime);
+	bApplyFirstShotAccuracy= WeaponProperties->bHasFirstShotAccuracy and bMinMultipliers and bMinSpread;
+}
+
+
+bool UWWeaponStateComponent::IsSpreadAtMinimum(float DeltaTime)
+{
+	float TimeSinceLastFired = GetWorld()->TimeSince(TimeLastFired);
+	float DecayThreshold = 60.0f/WeaponProperties->WeaponHandling.RoundsPerMinute*1.5;
+	if (TimeLastFired>0 and TimeSinceLastFired>DecayThreshold)
+	{
+		AccumulatedSpreadAngle = FMath::Max(0,
+			AccumulatedSpreadAngle-(WeaponProperties->WeaponHandling.SpreadAngleAccumulationDecayPerSecond*DeltaTime)); 
+		if (AccumulatedSpreadAngle < KINDA_SMALL_NUMBER) AccumulatedSpreadAngle = 0;
+	}
+	return AccumulatedSpreadAngle <= KINDA_SMALL_NUMBER;
+}
+
+bool UWWeaponStateComponent::AreMultipliersAtMinimum(float DeltaTime)
+{
+	return false;
+}
+
---
+++ Source/Whiplash/Public/Weapons/WWeaponStateComponent.h
@@ -0,0 +1,62 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Weapons/WWeaponDA.h"
+
+///////////////////////////////////////////
+#include "WWeaponStateComponent.generated.h"
+
+class UCharacterMovementComponent;
+class AWPlayerController;
+class AWCharacter;
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class WHIPLASH_API UWWeaponStateComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UWWeaponStateComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void InitializeComponent() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	TWeakObjectPtr<AWCharacter> WCachedPawn;
+	TWeakObjectPtr<UCharacterMovementComponent> WCachedCMC;// remove this can get CMC through character;
+	
+	TObjectPtr<UWWeaponDA> WeaponProperties;
+	TObjectPtr<AActor> Mesh;
+	
+	float TimeLastFired = 0;
+	float TimeLastEquipped = 0;
+	int32 CurrentMagazineAmmo=0;
+	int32 CurrentReserveAmmo=0;
+	float AccumulatedSpreadAngle=0;
+	float AccumulatedSpreadAngleMultiplier=1;
+	bool  bHas1InTheChamber=false;
+	bool  bApplyFirstShotAccuracy=false;
+	float CurrentMultiplier_StandingStill=1;
+	float CurrentMultiplier_Crouching=1;
+	float CurrentMultiplier_Falling=1;
+	
+	float FireIntervalSeconds =0;//calculated once in EquipWeapon as 60.0/sec also in CanFire fucntion when implmented 
+	bool  bMinSpread=false;
+	bool  bMinMultipliers=false;
+	
+	
+	
+	bool IsSpreadAtMinimum(float DeltaTime);
+	bool AreMultipliersAtMinimum(float DeltaTime);
+	
+	
+	
+
+public:
+	
+};
